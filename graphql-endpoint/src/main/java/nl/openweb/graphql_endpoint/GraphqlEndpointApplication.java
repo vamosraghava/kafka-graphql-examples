9b99909c73a2816f676de514bb00fe3738eba509
Gerard Klijs <g.klijs@gmail.com>
2019-10-23 21:31:29 +0200

    implement java graphql endpoint

---
+++ graphql-endpoint/src/main/java/nl/openweb/graphql_endpoint/GraphqlEndpointApplication.java
@@ -0,0 +1,14 @@
+package nl.openweb.graphql_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class GraphqlEndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlEndpointApplication.class, args);
+    }
+}
